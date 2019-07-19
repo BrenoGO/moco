27bf39467b85d67d8b3ce187f67fa3b104d8caa0
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2019-07-19 10:45:19 -0300

    secure changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,8 @@
 "use strict"
 const app = require('./config/express')();
 const PORT = app.get('port');
+const { dbURL } = require('./config/private');
 
-const url = 'mongodb://BrenoGaudereto:1q2w3e@ds253017.mlab.com:53017/money-controller';
-
-require('./config/db')(url);
+require('./config/db')(dbURL);
 
 app.listen(PORT, () => {console.log(`Server listening to port ${PORT}`)});
