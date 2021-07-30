ef3a39fb4e38acd7820c13ac263a03fa9f4926e1
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2021-07-30 18:32:42 -0300

    instalando antd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import './App.css';
+import 'antd/dist/antd.css';
 
 import auth from './services/Auth';
 
