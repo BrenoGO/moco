2a2df9f5f5bacf78243f5872f6a46d6b2c4f0186
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2022-11-08 11:37:17 -0300

    trying to trigger deployment

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -4,7 +4,7 @@ app.get('/api', (req, res) => {
   const path = '/api/item/1234';
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello test! Go to item: <a href="${path}">${path}</a>`);
 });
 
 module.exports = app;
