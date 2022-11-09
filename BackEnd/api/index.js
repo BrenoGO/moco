12430a1aa8379a34fe534a662f695d3f2ebe3d7f
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2022-11-09 10:17:34 -0300

    pointing to moco server

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,15 +1,3 @@
-const app = require('express')();
-
-app.get('/', (req, res) => {
-  const path = '/api/item/1234';
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+const app = require('../server');
 
 module.exports = app;
