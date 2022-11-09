9bcb6ac190fa649af14077b96ac1e85bfd653e9f
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2022-11-09 10:15:25 -0300

    test without api

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,13 +1,13 @@
 const app = require('express')();
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   const path = '/api/item/1234';
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+app.get('/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
