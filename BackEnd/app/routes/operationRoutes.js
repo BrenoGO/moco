e18f14403746f4fc7c9daa8305b483f9ccf60ef7
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2023-12-20 09:22:51 -0300

    fix: route for operations search

--- BackEnd/app/routes/operationRoutes.js
+++ BackEnd/app/routes/operationRoutes.js
@@ -4,7 +4,7 @@ const { auth } = require('../../config/auth');
 module.exports = function (app) {
   app.get('/operations/:id', auth.authenticate, operationController.index);
   app.get('/operations', auth.authenticate, operationController.all);
-  app.post('/registers/search', auth.authenticate, operationController.search);
+  app.post('/operations/search', auth.authenticate, operationController.search);
   app.post('/operations', auth.authenticate, operationController.store);
   app.delete('/operations/clearAll', auth.authenticate, operationController.clearAll);
   app.delete('/operations/:id', auth.authenticate, operationController.delete);
